7a2894d1d4979e3621b54765433123cf72a8b051
Florin <florin12@gmx.de>
2016-12-13 03:18:19 +0100

    Hashtag Funktion hinzugefügt und Design leicht geändert

--- app/src/main/java/com/example/blatt06/MessageDetailsActivity.java
+++ app/src/main/java/com/example/blatt06/MessageDetailsActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.regex.*;
+
+
 public class MessageDetailsActivity extends AppCompatActivity {
     private TextView tvName;
     private TextView tvMessage;
+    private TextView tvHashtag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +19,21 @@ public class MessageDetailsActivity extends AppCompatActivity {
         Intent displayIntent = getIntent();
         String nameValue = displayIntent.getStringExtra("name");
         String messageValue = displayIntent.getStringExtra("message");
+        String hashtagValue = "";
+
+        Pattern pattern = Pattern.compile("#(\\S+)");
+        Matcher matcher = pattern.matcher(messageValue);
+        while (matcher.find()) {
+
+            hashtagValue += matcher.group(1) + ", ";
+        }
+
         tvName =(TextView) findViewById(R.id.displayNameTextView);
         tvMessage =(TextView) findViewById(R.id.displayMessageTextView);
+        tvHashtag = (TextView) findViewById(R.id.displayHashtagTextView);
         tvName.setText(nameValue);
         tvMessage.setText(messageValue);
+        tvHashtag.setText(hashtagValue);
 
     }
 }
